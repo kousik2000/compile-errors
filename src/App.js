e16774970d8d2d5a27da40afe63eddd4c837accf
kousik2000 <kousik.ramachandruni@gmail.com>
2023-02-19 19:02:01 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import './App.css'
+import Header from './components/Header'
+import TodoList from './components/TodoList'
+import Notes from './components/Notes'
+import Remainder from './components/Remainder'
+import StopWatch from './components/StopWatch'
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={TodoList} />
+      <Route exact path="/notes" component={Notes} />
+      <Route exact path="/remainder" component={Remainder} />
+      <Route exact path="/stopwatch" component={StopWatch} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,44 @@
+import './index.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+const TodoItem = props => {
+  const {todoDetails, onClickDeleteButton} = props
+  const {id, addTask, date} = todoDetails
+
+  const onDelete = () => {
+    onClickDeleteButton(id)
+  }
+
+  return (
+    // <div className="todoListItem">
+    //   <h1>{addTask}</h1>
+    //   <p>{date}</p>
+    // </div>
+    <li className="todo-item-container">
+      <input
+        type="checkbox"
+        id="checkbox"
+        className="checkbox-input"
+        onChange={event => {
+          const checkboxLabel = event.target.nextElementSibling
+          checkboxLabel.classList.toggle('checked')
+        }}
+      />
+      <div className="label-container">
+        <label for="checkbox" className="checkbox-label">
+          {addTask}
+        </label>
+        <div className="date-delete-container">
+          <p className="task">{date}</p>
+          <button className="delete-button">
+            <div className="delete-icon-container" onClick={onDelete}>
+              <i className="fa-solid fa-trash-can delete-icon"></i>
+            </div>
+          </button>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default TodoItem
